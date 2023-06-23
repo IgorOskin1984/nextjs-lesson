da147d27cc4e0580e1a7403dedaceff7cf0103f2
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-23 18:33:54 +0300

    Создали ф-ю для чтения данных из "data base"

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,7 +1,21 @@
 import { PageWrapper } from 'components/PageWrapper/PageWrapper';
 import { getLayout } from 'components/Layout/BaseLayout/BaseLayout';
+import path from 'path';
+import fs from 'fs/promises'
 
 export const getStaticProps = async () => {
+
+	const getParseData = async () => {
+		const filePath = path.join(process.cwd(), 'public', 'staticData.json')
+
+		try {
+			const jsonData = await fs.readFile(filePath)
+			return JSON.parse(jsonData.toString())
+		} catch (err) {
+			return { title: 'no title' }
+		}
+
+	}
 
 	const title = ''
 
